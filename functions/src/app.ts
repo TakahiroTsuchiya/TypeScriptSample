687f319268ab8746a492732c08290b8c830ba7d2
TakahiroTsuchiya22 <takahirotsuchiya1984@icloud.com>
2018-04-04 14:59:42 +0900

    🐛  :bug: Fix Crash apps
    Corresponds to the problem that the application does not start up

--- functions/src/app.ts
+++ functions/src/app.ts
@@ -3,6 +3,10 @@ import express = require('express')
 const app = express()
 
 app.get('/', (req: express.Request, res: express.Response) => {
-    console.log('get access' + req.body.dataName)
+    console.log('get access baseUrl : ' + req.baseUrl)
     res.send({'param': 'response from typescript'})
+})
+
+const server = app.listen(process.env.PORT || 3000, () => {
+    console.log('Listening on port ' + server.address().port)
 })
