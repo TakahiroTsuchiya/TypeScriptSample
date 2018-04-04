99217546d193fda7c54d5977cbca43c791b87816
TakahiroTsuchiya22 <takahirotsuchiya1984@icloud.com>
2018-04-04 15:43:43 +0900

    👍  :+1: add sample APIs

--- functions/src/app.ts
+++ functions/src/app.ts
@@ -1,4 +1,4 @@
-import express = require('express')
+import * as express from 'express'
 
 const app = express()
 
@@ -6,6 +6,8 @@ app.get('/', (req: express.Request, res: express.Response) => {
     console.log('get access baseUrl : ' + req.baseUrl)
     res.send({'param': 'response from typescript'})
 })
+
+app.use(require('./routes'))
 
 const server = app.listen(process.env.PORT || 3000, () => {
     console.log('Listening on port ' + server.address().port)
