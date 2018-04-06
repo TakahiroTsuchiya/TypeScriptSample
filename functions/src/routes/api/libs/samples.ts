863f4faec09687d15b2012a1814ddd9c2742852f
TakahiroTsuchiya22 <takahirotsuchiya1984@icloud.com>
2018-04-06 11:46:31 +0900

    ♻️  :recycle: Changed directory structure

--- functions/src/routes/api/libs/samples.ts
+++ functions/src/routes/api/libs/samples.ts
@@ -4,7 +4,7 @@ const router = express.Router()
 
 // $domian[:$port]/api/sample
 router.get('/sample', (req, res, next) => {
-    res.json({'sample path': 'routes/api/sample.ts'})
+    res.json({'sample path': 'routes/api/libs/samples.ts'})
 })
 
 module.exports = router
