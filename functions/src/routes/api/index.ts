3ed1cd375d548b49dc78c8c0d3ee945c7b43b00f
TakahiroTsuchiya22 <takahirotsuchiya1984@icloud.com>
2018-04-06 14:53:48 +0900

    👍 :+1: add "cron.js" samples

--- functions/src/routes/api/index.ts
+++ functions/src/routes/api/index.ts
@@ -4,5 +4,6 @@ const router = express.Router()
 
 router.use('/', require('./libs/moments'))
 router.use('/', require('./libs/samples'))
+router.use('/', require('./libs/crons'))
 
 module.exports = router
