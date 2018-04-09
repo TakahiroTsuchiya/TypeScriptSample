3d13195f7ec0aed7421aea975fc1c03b3927e5f1
TakahiroTsuchiya22 <sample@sample.com>
2018-04-09 18:33:19 +0900

    👍 :+1: add "fs.js" samples

--- functions/src/routes/api/index.ts
+++ functions/src/routes/api/index.ts
@@ -7,5 +7,6 @@ router.use('/', require('./libs/samples'))
 router.use('/', require('./libs/crons'))
 router.use('/', require('./libs/request_promise'))
 router.use('/', require('./libs/promise'))
+router.use('/', require('./libs/fs'))
 
 module.exports = router
